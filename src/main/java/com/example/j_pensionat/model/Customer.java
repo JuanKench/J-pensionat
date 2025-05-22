ab0ac2fa3f2683b2aa9153d0c547d4d44cfc39d0
Josef Hedblom <hedblomjosef@gmail.com>
2025-05-22 12:34:01 +0200

    Add validation to Customer model.

--- src/main/java/com/example/j_pensionat/model/Customer.java
+++ src/main/java/com/example/j_pensionat/model/Customer.java
@@ -1,25 +1,47 @@
 package com.example.j_pensionat.model;
 
-
+import jakarta.validation.constraints.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Customer {
 
     @Id
-    @GeneratedValue
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotBlank(message = "Förnamn får inte vara tomt")
+    @Size(max = 50, message = "Förnamn får vara max 50 tecken")
+    @Pattern(regexp = "^[A-Za-zÅÄÖåäö\\s\\-]+$", message = "Förnamnet får endast innehålla bokstäver och mellanslag.")
     private String firstName;
+
+    @NotBlank(message = "Efternamn får inte vara tomt")
+    @Size(max = 50, message = "Efternamn får vara max 50 tecken")
+    @Pattern(regexp = "^[A-Za-zÅÄÖåäö\\s\\-]+$", message = "Efternamn får endast innehålla bokstäver och mellanslag.")
     private String lastName;
-    private int phoneNumber;
-    private boolean hasPaid; //det borde vara här men det finns inget krav på det
+
+    @NotBlank(message = "Telefonnummer får inte vara tomt")
+    @Pattern(
+            regexp = "^(\\+46|0)[1-9]\\d{6,9}$",
+            message = "Ogiltigt telefonnummer. Ex: 0701234567 eller +46701234567"
+    )
+    private String phoneNumber;
+
+    @NotBlank(message = "E-post får inte vara tomt")
+    @Email(message = "Ogitlig e-poestadress")
     private String email;
+
+    @NotBlank(message = "Adress får inte vara tomt")
     private String address;
+
+    private boolean hasPaid;
     //private String notes; // allergi saker
 
 }
