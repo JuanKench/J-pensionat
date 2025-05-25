1872ed8a8c765cc815679cda67c22263ffc4533b
Havrem <144966173+Havrem@users.noreply.github.com>
2025-05-25 14:25:09 +0200

    Fix merge

--- src/main/java/com/example/j_pensionat/model/Order.java
+++ src/main/java/com/example/j_pensionat/model/Order.java
@@ -1,6 +1,10 @@
 package com.example.j_pensionat.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,11 +19,20 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull(message = "Startdatum måste anges")
     private LocalDate startDate;
+
+    @NotNull(message = "Slutdatum måste anges")
+    @FutureOrPresent(message = "Slutdatum måste vara idag eller senare")
     private LocalDate endDate;
+
+    @NotNull(message = "Kund måste anges")
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
+
+    @NotNull(message = "Rum måste anges")
     @ManyToOne
     @JoinColumn(name = "room_id")
     private Room room;
@@ -27,6 +40,11 @@ public class Order {
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LineItem> lineItems;
+
+    //TODO: Saknas funktionalitet för det här.
+//    @Min(value = 1, message = "Minst 1 gäst måste bokas")
+//    @Max(value = 4, message = "Maximalt 4 gäster per boking")
+//    private int numGuests;
 
     public Order(LocalDate startDate, LocalDate endDate, Customer customer, Room room, String notes, List<LineItem> lineItems) {
         this.startDate = startDate;
